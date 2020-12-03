c079ec686bd8ad5502a3eb43b065e03d5083e1db
Cooper Anderson <cooper@cooperstuff.ninja>
2020-12-02 17:07:53 -0800

    Complete day 2 part 1

--- day2.py
+++ day2.py
@@ -1,5 +1,17 @@
+from re import match
+
+file = open("data/day2", 'r')
+lines = file.readlines()
+pattern = "([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)"
+
 def part1():
-    pass
+    valid = 0
+    for line in lines:
+        g = match(pattern, line).groups()
+        count = g[3].count(g[2])
+        if count >= int(g[0]) and count <= int(g[1]):
+            valid += 1
+    return valid
 
 def part2():
     pass
