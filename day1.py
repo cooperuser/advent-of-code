7b23c755ae9e86412ee75a00857f6612c7e0004d
Cooper Anderson <cooper@cooperstuff.ninja>
2020-12-02 16:23:05 -0800

    Complete day 1 part 2

--- day1.py
+++ day1.py
@@ -1,4 +1,4 @@
-file = open("data/day1a", 'r')
+file = open("data/day1", 'r')
 numbers = [int(line) for line in file.readlines()]
 length = len(numbers)
 
@@ -11,6 +11,18 @@ def part1():
                 return a * b
     return -1
 
+def part2():
+    for i in range(length):
+        for j in range(i + 1, length):
+            for k in range(j + 1, length):
+                a = numbers[i]
+                b = numbers[j]
+                c = numbers[k]
+                if a + b + c == 2020:
+                    return a * b * c
+    return -1
+
 if __name__ == "__main__":
     print(part1())
+    print(part2())
 
