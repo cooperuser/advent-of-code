5bfca25e17a50fc804143228a605fcca2fd53505
Cooper Anderson <cooper@cooperstuff.ninja>
2020-12-05 21:22:05 -0800

    Complete day 6

--- day06.py
+++ day06.py
@@ -2,16 +2,43 @@ from day import Day
 
 class Day06(Day):
 	day = 6
-	title = ""
+	title = "Custom Customs"
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 	def setup(self, lines) -> None:
-		pass
+		self.groups = []
+		group = []
+		for line in lines:
+			if line.strip() == "":
+				self.groups.append(group)
+				group = []
+				continue
+			group.append(line.strip())
+		self.groups.append(group)
 
 	def part1(self) -> int:
-		return -1
+		count = 0
+		for group in self.groups:
+			combined = "".join(group)
+			unique = set()
+			for c in Day06.alphabet:
+				if c in combined:
+					unique.add(c)
+			count += len(unique)
+		return count
 
 	def part2(self) -> int:
-		return -1
+		count = 0
+		for group in self.groups:
+			unique = set()
+			for c in Day06.alphabet:
+				for person in group:
+					if c not in person:
+						break
+				else:
+					unique.add(c)
+			count += len(unique)
+		return count
 
 
 if __name__ == "__main__":
