720638c645a5b15d26ffdc6d7c0e744cd14636b0
Cooper Anderson <cooper@cooperstuff.ninja>
2020-12-08 22:33:39 -0800

    Complete day 9

--- day09.py
+++ day09.py
@@ -1,18 +1,53 @@
-from day import Day
+from day import *
 
 class Day09(Day):
 	day = 9
-	title = ""
+	title = "Encoding Error"
 
-	def setup(self, lines) -> None:
-		pass
+	def setup(self, lines: List[str]) -> None:
+		self.lines = [int(line.strip()) for line in lines]
+	
+	def find_error(self, preamble: int):
+		queue = []
+		pointer = 0
+		for _ in range(preamble):
+			queue.append(self.lines[pointer])
+			pointer += 1
+		for _ in range(len(self.lines) - preamble):
+			if not Day09.is_sum(self.lines[pointer], queue):
+				return pointer
+			queue.pop(0)
+			queue.append(self.lines[pointer])
+			pointer += 1
+		return -1
 
 	def part1(self) -> int:
-		return -1
+		self.index = self.find_error(25)
+		self.answer = self.lines[self.index]
+		return self.answer
 
 	def part2(self) -> int:
+		low = 0
+		high = 1
+		total = self.lines[low]
+		while total < self.answer:
+			total += self.lines[high]
+			high += 1
+			while total > self.answer:
+				total -= self.lines[low]
+				low += 1
+			if total == self.answer:
+				subarray = self.lines[low:high]
+				return min(subarray) + max(subarray)
 		return -1
 
+	@staticmethod
+	def is_sum(number: int, queue: List[int]):
+		for i in range(len(queue) - 1):
+			if number - queue[i] in queue:
+				return True
+		return False
+	
 
 if __name__ == "__main__":
 	d = Day09()
