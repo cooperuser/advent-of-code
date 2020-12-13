8eb14757ee3686406616b2942d9ad5e34f55c402
Cooper Anderson <cooper@cooperstuff.ninja>
2020-12-13 00:59:26 -0800

    Complete day 13

--- day13.py
+++ day13.py
@@ -1,17 +1,43 @@
 from day import *
+from math import gcd
 
 class Day13(Day):
 	day = 13
-	title = ""
+	title = "Shuttle Search"
 
 	def setup(self, lines: List[str]) -> None:
-		pass
+		self.earliest = int(lines[0].strip())
+		self.busses = {}
+		self.lcm = 1
+		for index, raw in enumerate(lines[1].strip().split(',')):
+			if raw != 'x':
+				bus = int(raw)
+				self.busses[bus] = index
+				self.lcm = self.lcm * bus // gcd(self.lcm, bus)
 
 	def part1(self) -> int:
-		return -1
+		smallest = -1
+		bus = 0
+		for b in self.busses:
+			times = self.earliest // b + 1
+			wait = times * b - self.earliest
+			if wait < smallest or smallest == -1:
+				smallest = wait
+				bus = b
+		return bus * smallest
 
 	def part2(self) -> int:
-		return -1
+		lcm = 0
+		offset = 0
+		for bus, index in self.busses.items():
+			if not index:
+				lcm = bus
+			for n in range(1, 10000):
+				if (offset + lcm*n - index) % bus == 0:
+					offset += lcm*n
+					lcm *= bus // gcd(lcm, bus)
+					break
+		return self.lcm - offset
 
 
 if __name__ == "__main__":
