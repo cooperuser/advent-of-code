199352ab4167f0adf63292dfe589774f7cfe7394
Cooper b. Anderson <andersc7@rose-hulman.edu>
2022-12-04 23:12:53 -0500

    Add template day module

---
+++ src/day00/mod.rs
@@ -0,0 +1,43 @@
+pub const INPUT: &str = include_str!("input.txt");
+pub const SAMPLE: &str = include_str!("input_sample.txt");
+pub const SAMPLE_A: i32 = 0;
+pub const SAMPLE_B: i32 = 0;
+
+#[derive(Default)]
+pub struct Solution {
+    raw: Vec<String>,
+}
+
+impl Solution {
+    pub fn new(raw: Vec<String>) -> Self {
+        Self {
+            raw: raw.clone(),
+            ..Default::default()
+        }
+    }
+
+    pub fn part_a(&self) -> i32 {
+        0
+    }
+
+    pub fn part_b(&self) -> i32 {
+        0
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn part_a() {
+        let solution = Solution::new(crate::split(SAMPLE));
+        assert_eq!(solution.part_a(), SAMPLE_A);
+    }
+
+    #[test]
+    fn part_b() {
+        let solution = Solution::new(crate::split(SAMPLE));
+        assert_eq!(solution.part_b(), SAMPLE_B);
+    }
+}
