f75f2e059d49fe61495aa8c7f889dfe3182288f6
Cooper b. Anderson <andersc7@rose-hulman.edu>
2022-12-06 00:09:33 -0500

    Day 06

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ mod day02;
 mod day03;
 mod day04;
 mod day05;
+mod day06;
 
 fn main() {
-    use day05::*;
+    use day06::*;
 
     let sample = Solution::new(split(SAMPLE));
     let real = Solution::new(split(INPUT));
