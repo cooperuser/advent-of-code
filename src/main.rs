de28d0cff48a7815ba3379973193ad6cf3b55827
Cooper b. Anderson <andersc7@rose-hulman.edu>
2023-11-30 08:18:57 -0800

    Prepare main.rs for day 01

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,7 @@
 mod day01;
-mod day02;
-mod day03;
-mod day04;
-mod day05;
-mod day06;
-mod day07;
-mod day08;
-mod day09;
-mod day10;
-mod day11;
-mod day12;
-mod day13;
-mod day14;
-mod day15;
-mod day16;
-mod day17;
-mod day18;
-mod day19;
-mod day20;
-mod day21;
-mod day22;
-mod day23;
-mod day24;
-mod day25;
 
 fn main() {
-    use day25::*;
+    use day01::*;
 
     let sample = Solution::new(split(SAMPLE));
     let real = Solution::new(split(INPUT));
