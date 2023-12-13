04979be6a92ca340a3a0f669286d2f538438755d
Cooper b. Anderson <andersc7@rose-hulman.edu>
2023-12-13 17:07:41 -0600

    Cleanup day 13

--- src/day13/mod.rs
+++ src/day13/mod.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use std::collections::HashSet;
 
 pub const INPUT: &str = include_str!("input.txt");
@@ -15,7 +17,6 @@ pub struct Solution {
 
 #[derive(Debug)]
 struct Pattern {
-    points: HashSet<(i64, i64)>,
     size: (i64, i64),
     rows: Vec<i64>,
     cols: Vec<i64>,
@@ -25,38 +26,6 @@ struct Pattern {
 enum RowCol {
     Row(i64),
     Col(i64),
-}
-
-impl Pattern {
-    fn debug(&self, spot: (i64, i64), slice_row: Option<&i64>, slice_col: Option<&i64>) -> Vec<String> {
-        let mut lines = Vec::new();
-        if let Some(col) = slice_col {
-            lines.push(format!("{}><{}", " ".repeat(*col as usize), " ".repeat((self.size.1 - *col - 1) as usize)));
-        } else {
-            lines.push(" ".repeat(self.size.1 as usize + 1));
-        }
-        for row in 0..self.size.0 {
-            let mut line = "".to_string();
-            if Some(&(row + 1)) == slice_row {
-                line = format!("{}v", line);
-            } else if Some(&row) == slice_row {
-                line = format!("{}^", line);
-            } else {
-                line = format!("{} ", line);
-            }
-            for col in 0..self.size.1 {
-                if (row, col) == spot {
-                    line = format!("{}O", line);
-                } else if self.points.contains(&(row, col)) {
-                    line = format!("{}#", line);
-                } else {
-                    line = format!("{}.", line);
-                }
-            }
-            lines.push(line);
-        }
-        lines
-    }
 }
 
 fn count_row(points: &HashSet<(i64, i64)>, row: i64, max: i64) -> i64 {
@@ -123,7 +92,6 @@ impl Solution {
             patterns.push(Pattern {
                 rows: (0..size.0).map(|row| count_row(&points, row, size.1)).collect(),
                 cols: (0..size.1).map(|col| count_col(&points, col, size.0)).collect(),
-                points,
                 size,
             });
         }
@@ -166,17 +134,17 @@ impl Solution {
                     let cols: HashSet<RowCol> = find_lines(&cols).iter().map(|r| RowCol::Col(*r)).collect();
                     let slices: HashSet<RowCol> = rows.union(&cols).cloned().collect();
 
-                    for slice in slices.difference(&orig) {
+                    if let Some(slice) = slices.difference(&orig).next() {
                         match slice {
                             RowCol::Row(r) => {
                                 sum += r * 100;
-                                continue 'pattern;
                             },
                             RowCol::Col(c) => {
                                 sum += c;
-                                continue 'pattern;
                             },
                         }
+
+                        continue 'pattern;
                     }
                 }
             }
