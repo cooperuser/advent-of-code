1f99d3dfc653ea49f3aafeeb95174bcc5065eeb1
Cooper b. Anderson <andersc7@rose-hulman.edu>
2023-12-18 23:55:46 -0600

    Day 19 part 1

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,10 @@ mod day15;
 mod day16;
 mod day17;
 mod day18;
+mod day19;
 
 fn main() {
-    use day18::*;
+    use day19::*;
 
     let sample_a = Solution::new(split(SAMPLE_A));
     let sample_b = Solution::new(split(SAMPLE_B));
