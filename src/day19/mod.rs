d583d01929df1ef64cf5901a24592bd32b2caadb
Cooper b. Anderson <andersc7@rose-hulman.edu>
2023-12-19 16:27:23 -0600

    Cleanup day 19 part 1

--- src/day19/mod.rs
+++ src/day19/mod.rs
@@ -14,17 +14,13 @@ pub struct Solution {
     ratings: Vec<HashMap<Category, i64>>,
 }
 
-enum Workflow {
-    Target(Target),
-    Workflow {
-        category: Category,
-        operation: Operation,
-        predicate: i64,
-        target: Target,
-    },
+#[derive(Debug)]
+struct Workflow {
+    operation: Operation,
+    action: Action,
 }
 
-#[derive(PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 enum Category {
     Extremely,
     Musical,
@@ -46,12 +42,15 @@ impl FromStr for Category {
     }
 }
 
+#[derive(Debug)]
 enum Operation {
-    LessThan,
-    GreaterThan,
+    LessThan(Category, i64),
+    GreaterThan(Category, i64),
+    Conditionless,
 }
 
-enum Target {
+#[derive(Debug)]
+enum Action {
     Label(String),
     Reject,
     Accept,
@@ -61,47 +60,33 @@ impl Solution {
     pub fn new(raw: Vec<String>) -> Self {
         let blocks: Vec<_> = raw.split(|line| line.is_empty()).collect();
         let mut workflows = HashMap::new();
+
         for line in blocks[0] {
             let mut workflow = Vec::new();
-            let (label, parts_raw) = line.split_once('{').unwrap();
-            for part in parts_raw.strip_suffix('}').unwrap().split(',') {
-                match part.split_once(':') {
-                    Some((rule, target)) => {
-                        let operation;
-                        let category;
-                        let value;
-                        if let Some((c, v)) = rule.split_once('<') {
-                            operation = Some(Operation::LessThan);
-                            category = c.parse().ok();
-                            value = v.parse().ok();
+            let (label, clauses) = line.split_once('{').unwrap();
+            for clause in clauses.strip_suffix('}').unwrap().split(',') {
+                let (operation, action) = match clause.split_once(':') {
+                    Some((rule, action)) => {
+                        let operation = if let Some((c, v)) = rule.split_once('<') {
+                            Operation::LessThan(c.parse().unwrap(), v.parse().unwrap())
                         } else if let Some((c, v)) = rule.split_once('>') {
-                            operation = Some(Operation::GreaterThan);
-                            category = c.parse().ok();
-                            value = v.parse().ok();
+                            Operation::GreaterThan(c.parse().unwrap(), v.parse().unwrap())
                         } else {
-                            panic!();
-                        }
-                        let target = match target {
-                            "R" => Target::Reject,
-                            "A" => Target::Accept,
-                            _ => Target::Label(target.to_string()),
+                            panic!("unknown operation: {rule}")
                         };
+                        (operation, action)
+                    }
+                    None => (Operation::Conditionless, clause),
+                };
 
-                        workflow.push(Workflow::Workflow {
-                            category: category.unwrap(),
-                            operation: operation.unwrap(),
-                            predicate: value.unwrap(),
-                            target,
-                        })
+                workflow.push(Workflow {
+                    operation,
+                    action: match action {
+                        "R" => Action::Reject,
+                        "A" => Action::Accept,
+                        _ => Action::Label(action.to_string()),
                     }
-                    None => {
-                        workflow.push(Workflow::Target(match part {
-                            "R" => Target::Reject,
-                            "A" => Target::Accept,
-                            _ => Target::Label(part.to_string()),
-                        }));
-                    }
-                }
+                })
             }
             workflows.insert(label.to_string(), workflow);
         }
@@ -119,6 +104,7 @@ impl Solution {
             }
             ratings.push(rating);
         }
+
         Self {
             raw: raw.clone(),
             workflows,
@@ -138,62 +124,26 @@ impl Solution {
     }
 
     pub fn part_b(&self) -> Option<i64> {
-        let mut sum = 0;
-        for x in 1..=4000 {
-            for m in 1..=4000 {
-                println!("x: {x}, m: {m}, sum: {sum}");
-                for a in 1..=4000 {
-                    for s in 1..=4000 {
-                        let rating = HashMap::from([
-                            (Category::Extremely, x),
-                            (Category::Musical, m),
-                            (Category::Aerodynamic, a),
-                            (Category::Shiny, s),
-                        ]);
-                        if self.test_rating(&rating) {
-                            sum += x + m + a + s;
-                        }
-                    }
-                }
-            }
-        }
-        Some(sum)
+        None
     }
 
     fn test_rating(&self, rating: &HashMap<Category, i64>) -> bool {
         let mut label = "in".to_string();
         'outer: loop {
             let workflow = self.workflows.get(&label).unwrap();
-            for step in workflow {
-                match step {
-                    Workflow::Workflow {
-                        category,
-                        operation,
-                        predicate,
-                        target,
-                    } => {
-                        let value = rating.get(category).unwrap();
-                        let success = match operation {
-                            Operation::LessThan => value < predicate,
-                            Operation::GreaterThan => value > predicate,
-                        };
-                        if success {
-                            match target {
-                                Target::Label(target) => label = target.clone(),
-                                Target::Reject => break 'outer false,
-                                Target::Accept => break 'outer true,
-                            }
-                            break;
-                        }
+            for Workflow { operation, action } in workflow {
+                let success = match operation {
+                    Operation::LessThan(c, v) => rating.get(c).unwrap() < v,
+                    Operation::GreaterThan(c, v) => rating.get(c).unwrap() > v,
+                    Operation::Conditionless => true,
+                };
+                if success {
+                    match action {
+                        Action::Label(l) => label = l.clone(),
+                        Action::Reject => break 'outer false,
+                        Action::Accept => break 'outer true,
                     }
-                    Workflow::Target(target) => {
-                        match target {
-                            Target::Label(target) => label = target.clone(),
-                            Target::Reject => break 'outer false,
-                            Target::Accept => break 'outer true,
-                        }
-                        break;
-                    }
+                    break;
                 }
             }
         }
