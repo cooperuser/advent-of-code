9889469d9d5608099542ff7dc14d04c0f1058f69
Cooper b. Anderson <cooper@cooperuser.dev>
2024-12-02 01:20:27 -0600

    Prepare for day 01

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,10 +1,8 @@
-#![allow(dead_code)]
-
 pub const INPUT: &str = include_str!("input.txt");
 pub const SAMPLE_A: &str = include_str!("input_sample.txt");
 pub const SAMPLE_B: &str = SAMPLE_A;
-pub const ANSWER_A: i64 = 209;
-pub const ANSWER_B: i64 = 281;
+pub const ANSWER_A: i64 = 0;
+pub const ANSWER_B: i64 = 0;
 
 #[derive(Default)]
 pub struct Solution {
@@ -20,61 +18,11 @@ impl Solution {
     }
 
     pub fn part_a(&self) -> Option<i64> {
-        let mut sum = 0;
-        for line in &self.raw {
-            let mut first = 0;
-            let mut last = 0;
-            for char in line.chars() {
-                if !char.is_numeric() { continue; }
-                if first == 0 { first = char.to_digit(10).unwrap(); }
-                last = char.to_digit(10).unwrap();
-            }
-            sum += first * 10 + last;
-        }
-        Some(sum as i64)
+        None
     }
 
     pub fn part_b(&self) -> Option<i64> {
-        let mut sum = 0;
-        for line in &self.raw {
-            let mut nums: Vec<u32> = Vec::new();
-            let mut three: Vec<String> = Vec::new();
-            let mut four: Vec<String> = Vec::new();
-            let mut five: Vec<String> = Vec::new();
-
-            for char in line.chars() {
-                if char.is_numeric() {
-                    nums.push(char.to_digit(10).unwrap());
-                    continue;
-                }
-                three.push(char.to_string());
-                four.push(char.to_string());
-                five.push(char.to_string());
-
-                if three.len() > 3 { three.remove(0); }
-                if four.len() > 4 { four.remove(0); }
-                if five.len() > 5 { five.remove(0); }
-
-                let three = three.join("");
-                let four = four.join("");
-                let five = five.join("");
-
-                if three == "one" { nums.push(1); }
-                else if three == "two" { nums.push(2); }
-                else if three == "six" { nums.push(6); }
-                if four == "zero" { nums.push(0); }
-                else if four == "four" { nums.push(4); }
-                else if four == "five" { nums.push(5); }
-                else if four == "nine" { nums.push(9); }
-                if five == "three" { nums.push(3); }
-                else if five == "seven" { nums.push(7); }
-                else if five == "eight" { nums.push(8); }
-            }
-            let first = nums.first().unwrap();
-            let last = nums.last().unwrap();
-            sum += first * 10 + last;
-        }
-        Some(sum as i64)
+        None
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,7 @@
 mod day01;
-mod day02;
-mod day03;
-mod day04;
-mod day05;
-mod day06;
-mod day07;
-mod day08;
-mod day09;
-mod day10;
-mod day11;
-mod day12;
-mod day13;
-mod day14;
-mod day15;
-mod day16;
-mod day17;
-mod day18;
-mod day19;
-mod day20;
 
 fn main() {
-    use day20::*;
+    use day01::*;
 
     let sample_a = Solution::new(split(SAMPLE_A));
     let sample_b = Solution::new(split(SAMPLE_B));
@@ -36,12 +17,12 @@ fn main() {
             let duration = start.elapsed();
             println!("part_a:\t{}", answer.unwrap());
             println!(" ** in:\t{:?}", duration);
-        },
+        }
         Some(received) => {
             println!("part_a: failed!");
             println!("\texpected: {}", ANSWER_A);
             println!("\treceived: {}", received);
-        },
+        }
         None => {
             println!("part_a: unsolved!");
         }
@@ -54,12 +35,12 @@ fn main() {
             let duration = start.elapsed();
             println!("part_b:\t{}", answer.unwrap());
             println!(" ** in:\t{:?}", duration);
-        },
+        }
         Some(received) => {
             println!("part_b: failed!");
             println!("\texpected: {}", ANSWER_B);
             println!("\treceived: {}", received);
-        },
+        }
         None => {
             println!("part_b: unsolved!");
         }
@@ -67,5 +48,9 @@ fn main() {
 }
 
 pub fn split(input: &str) -> Vec<String> {
-    input.trim_end().split('\n').map(|s| s.trim().to_string()).collect()
+    input
+        .trim_end()
+        .split('\n')
+        .map(|s| s.trim().to_string())
+        .collect()
 }
