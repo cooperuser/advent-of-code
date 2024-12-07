c980fd9f517bdd8300f1b270b20f8cd6c4083dc6
Cooper b. Anderson <cooper@cooperuser.dev>
2024-12-06 23:25:48 -0600

    Day 07

--- src/day07/mod.rs
+++ src/day07/mod.rs
@@ -1,28 +1,78 @@
+use std::collections::VecDeque;
+
 pub const INPUT: &str = include_str!("input.txt");
 pub const SAMPLE_A: &str = include_str!("input_sample.txt");
 pub const SAMPLE_B: &str = SAMPLE_A;
-pub const ANSWER_A: i64 = 0;
-pub const ANSWER_B: i64 = 0;
+pub const ANSWER_A: i64 = 3749;
+pub const ANSWER_B: i64 = 11387;
 
-#[derive(Default)]
 pub struct Solution {
     #[allow(dead_code)]
     raw: Vec<String>,
+    equations: Vec<(i64, Vec<i64>)>,
 }
 
 impl Solution {
     pub fn new(raw: Vec<String>) -> Self {
+        let mut equations = Vec::new();
+        for line in raw.iter() {
+            let (left, right) = line.split_once(':').unwrap();
+            let left = left.parse().unwrap();
+            let right = right
+                .split_whitespace()
+                .map(|num| num.parse().unwrap())
+                .collect();
+            equations.push((left, right));
+        }
         Self {
             raw: raw.clone(),
+            equations,
         }
     }
 
     pub fn part_a(&self) -> Option<i64> {
-        None
+        let mut sum = 0;
+        'equation: for equation in self.equations.iter() {
+            let len = equation.1.len();
+            let mut deque: VecDeque<(i64, usize)> = VecDeque::new();
+            deque.push_back((equation.1[0], 1));
+            while let Some(next) = deque.pop_front() {
+                if next.1 == len && next.0 == equation.0 {
+                    sum += equation.0;
+                    continue 'equation;
+                } else if next.1 == len {
+                    continue;
+                }
+
+                deque.push_back((next.0 + equation.1[next.1], next.1 + 1));
+                deque.push_back((next.0 * equation.1[next.1], next.1 + 1));
+            }
+        }
+        Some(sum)
     }
 
     pub fn part_b(&self) -> Option<i64> {
-        None
+        let mut sum = 0;
+        'equation: for equation in self.equations.iter() {
+            let len = equation.1.len();
+            let mut deque: VecDeque<(i64, usize)> = VecDeque::new();
+            deque.push_back((equation.1[0], 1));
+            while let Some(next) = deque.pop_front() {
+                if next.1 == len && next.0 == equation.0 {
+                    sum += equation.0;
+                    continue 'equation;
+                } else if next.1 == len {
+                    continue;
+                }
+
+                deque.push_back((next.0 + equation.1[next.1], next.1 + 1));
+                deque.push_back((next.0 * equation.1[next.1], next.1 + 1));
+                let right = equation.1[next.1];
+                let magnitude = (right as f64).log10().ceil() as u32;
+                deque.push_back((next.0 * 10i64.pow(magnitude) + right, next.1 + 1));
+            }
+        }
+        Some(sum)
     }
 }
 
