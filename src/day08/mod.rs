99e53591f2d7f3133c53837a07437297541ac950
Cooper b. Anderson <cooper@cooperuser.dev>
2024-12-07 23:30:24 -0600

    Day 08

--- src/day08/mod.rs
+++ src/day08/mod.rs
@@ -1,6 +1,12 @@
+use std::collections::HashMap;
+
+use crate::vector::{Vector, VectorSet};
+
 pub struct Day {
     #[allow(dead_code)]
     raw: Vec<String>,
+    antennae: HashMap<char, VectorSet>,
+    size: Vector,
 }
 
 impl crate::solution::Solution<i64> for Day {
@@ -9,21 +15,67 @@ impl crate::solution::Solution<i64> for Day {
             input: include_str!("input.txt").to_string(),
             sample_a: include_str!("input_sample.txt").to_string(),
             sample_b: include_str!("input_sample.txt").to_string(),
-            answer_a: 0,
-            answer_b: 0,
+            answer_a: 14,
+            answer_b: 34,
         }
     }
 
     fn new(raw: Vec<String>) -> Self {
-        Self { raw: raw.clone() }
+        let size = Vector::new_usize(raw[0].len(), raw.len());
+        let mut antennae = HashMap::new();
+        for (y, line) in raw.iter().enumerate() {
+            for (x, c) in line.chars().enumerate() {
+                if c == '.' {
+                    continue;
+                }
+                antennae
+                    .entry(c)
+                    .or_insert_with(|| VectorSet::new(size))
+                    .insert(Vector::new_usize(x, y));
+            }
+        }
+
+        Self {
+            raw: raw.clone(),
+            antennae,
+            size,
+        }
     }
 
     fn part_a(&self) -> Option<i64> {
-        None
+        let mut antinodes = VectorSet::new(self.size);
+        for spaces in self.antennae.values() {
+            for (i, first) in spaces.iter().enumerate() {
+                for second in spaces.iter().skip(i + 1) {
+                    let diff = second - first;
+                    antinodes.insert(first - diff);
+                    antinodes.insert(second + diff);
+                }
+            }
+        }
+        Some(antinodes.iter().count() as i64)
     }
 
     fn part_b(&self) -> Option<i64> {
-        None
+        let mut antinodes = VectorSet::new(self.size);
+        for spaces in self.antennae.values() {
+            for (i, first) in spaces.iter().enumerate() {
+                for second in spaces.iter().skip(i + 1) {
+                    let diff = second - first;
+                    let mut a = first;
+                    let mut b = second;
+                    while a.contained_in(Vector::zero(), self.size) {
+                        antinodes.insert(a);
+                        a -= diff;
+                    }
+                    while b.contained_in(Vector::zero(), self.size) {
+                        antinodes.insert(b);
+                        b += diff;
+                    }
+                }
+            }
+        }
+        Some(antinodes.iter().count() as i64)
     }
 }
 
