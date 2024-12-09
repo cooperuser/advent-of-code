097e478cdeb6575f743be741506749b5c8953d1c
Cooper b. Anderson <cooper@cooperuser.dev>
2024-12-09 00:45:20 -0600

    Joe's day 06 idea
    Only start each loop from the current position in the path

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use crate::direction::Direction;
 use crate::vector::{Vector, VectorMap, VectorSet};
 
@@ -10,10 +12,16 @@ pub struct Day {
 }
 
 impl Day {
-    fn compute_path(&self, extra: Option<Vector>) -> Option<VectorMap<Vec<bool>>> {
+    fn compute_path(
+        &self,
+        extra: Option<Vector>,
+        start: Vector,
+        facing: Direction,
+    ) -> Option<Vec<(Vector, Direction)>> {
         let mut seen: VectorMap<Vec<bool>> = VectorMap::new(self.size);
-        let mut facing = Direction::North;
-        let mut pos = self.start;
+        let mut path: Vec<(Vector, Direction)> = Vec::new();
+        let mut facing = facing;
+        let mut pos = start;
         loop {
             match seen.get(pos) {
                 Some(dirs) => {
@@ -29,10 +37,11 @@ impl Day {
             seen.get_mut(pos).unwrap()[facing as usize] = true;
             let next = pos + facing;
             if !next.contained_in(Vector::zero(), self.size) {
-                return Some(seen);
+                return Some(path);
             } else if self.grid.contains(next) || Some(next) == extra {
                 facing = facing.rotate_right();
             } else {
+                path.push((pos, facing));
                 pos = next;
             }
         }
@@ -74,15 +83,33 @@ impl crate::solution::Solution<i64> for Day {
     }
 
     fn part_a(&self) -> Option<i64> {
-        Some(self.compute_path(None).unwrap().len() as i64)
+        Some(
+            1 + self
+                .compute_path(None, self.start, Direction::North)
+                .unwrap()
+                .iter()
+                .map(|(pos, _)| *pos)
+                .collect::<HashSet<Vector>>()
+                .len() as i64,
+        )
     }
 
     fn part_b(&self) -> Option<i64> {
         let mut total = 0;
-        for (spot, _) in self.compute_path(None).unwrap() {
-            if spot == self.start {
+        let mut tried = VectorSet::new(self.size);
+        for (spot, facing) in self
+            .compute_path(None, self.start, Direction::North)
+            .unwrap()
+        {
+            if !tried.insert(spot + facing).unwrap()
+                || spot + facing == self.start
+                || !(spot + facing).contained_in(Vector::zero(), self.size)
+            {
                 continue;
-            } else if self.compute_path(Some(spot)).is_none() {
+            } else if self
+                .compute_path(Some(spot + facing), spot, facing)
+                .is_none()
+            {
                 total += 1;
             }
         }
