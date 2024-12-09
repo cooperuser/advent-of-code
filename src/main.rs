6ea996fb72c499946d2a605c549d3bf7160623c8
Cooper b. Anderson <cooper@cooperuser.dev>
2024-12-08 20:56:50 -0600

    Add commandline arguments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![feature(slice_split_once)]
 
-use solution::{Output, Solution};
+use solution::Solution;
 
 mod direction;
 mod solution;
@@ -17,69 +17,37 @@ mod day08;
 mod day09;
 
 fn main() {
-    let day = day::<day09::Day, i64>();
-    println!("parse :\t{:?}\n", day.duration);
+    let args: Vec<String> = std::env::args().collect();
+    let days = &[
+        day01::Day::run,
+        day02::Day::run,
+        day03::Day::run,
+        day04::Day::run,
+        day05::Day::run,
+        day06::Day::run,
+        day07::Day::run,
+        day08::Day::run,
+        day09::Day::run,
+    ];
 
-    match day.sample_a.part_a() {
-        Some(received) if received == day.answer_a => {
-            let start = std::time::Instant::now();
-            let answer = day.real.part_a();
-            let duration = start.elapsed();
-            println!("part_a:\t{}", answer.unwrap());
-            println!(" ** in:\t{:?}", duration);
+    match args.get(1) {
+        Some(arg) if arg == "all" => {
+            for (index, day) in days.iter().enumerate() {
+                println!("\n===== Day {} =====", index + 1);
+                day();
+            }
         }
-        Some(received) => {
-            println!("part_a: failed!");
-            println!("\texpected: {}", day.answer_a);
-            println!("\treceived: {}", received);
+        Some(arg) => {
+            let Ok(day) = arg.parse::<usize>() else {
+                return;
+            };
+            println!("===== Day {day} =====");
+            days[day - 1]()
         }
         None => {
-            println!("part_a: unsolved!");
+            println!("===== Day {} =====", days.len());
+            days.last().unwrap()()
         }
-    }
-
-    match day.sample_b.part_b() {
-        Some(received) if received == day.answer_b => {
-            let start = std::time::Instant::now();
-            let answer = day.real.part_b();
-            let duration = start.elapsed();
-            println!("part_b:\t{}", answer.unwrap());
-            println!(" ** in:\t{:?}", duration);
-        }
-        Some(received) => {
-            println!("part_b: failed!");
-            println!("\texpected: {}", day.answer_b);
-            println!("\treceived: {}", received);
-        }
-        None => {
-            println!("part_b: unsolved!");
-        }
-    }
-}
-
-struct Day<D: Solution<T>, T: Output> {
-    real: D,
-    sample_a: D,
-    sample_b: D,
-    answer_a: T,
-    answer_b: T,
-    duration: std::time::Duration,
-}
-
-fn day<D: Solution<T>, T: Output>() -> Day<D, T> {
-    let meta = D::meta();
-    let sample_a = D::new(split(meta.sample_a));
-    let sample_b = D::new(split(meta.sample_b));
-    let start = std::time::Instant::now();
-    let real = D::new(split(meta.input));
-    let duration = start.elapsed();
-    Day::<D, T> {
-        real,
-        sample_a,
-        sample_b,
-        answer_a: meta.answer_a,
-        answer_b: meta.answer_b,
-        duration,
     }
 }
 
--- src/solution.rs
+++ src/solution.rs
@@ -1,9 +1,4 @@
-pub trait Output {}
-impl Output for i64 {}
-impl Output for String {}
-impl Output for Box<dyn Output + '_> {}
-
-pub trait Solution<T: Output> {
+pub trait Solution<T: std::fmt::Display + Eq> {
     fn meta() -> Meta<T>
     where
         Self: Sized;
@@ -12,6 +7,54 @@ pub trait Solution<T: Output> {
         Self: Sized;
     fn part_a(&self) -> Option<T>;
     fn part_b(&self) -> Option<T>;
+    fn run()
+    where
+        Self: Sized,
+    {
+        let meta = Self::meta();
+        let sample_a = Self::new(crate::split(meta.sample_a));
+        let sample_b = Self::new(crate::split(meta.sample_b));
+        let start = std::time::Instant::now();
+        let real = Self::new(crate::split(meta.input));
+        let duration = start.elapsed();
+
+        println!("parse :\t{:?}\n", duration);
+        match sample_a.part_a() {
+            Some(received) if received == meta.answer_a => {
+                let start = std::time::Instant::now();
+                let answer = real.part_a();
+                let duration = start.elapsed();
+                println!("part_a:\t{}", answer.unwrap());
+                println!(" ** in:\t{:?}", duration);
+            }
+            Some(received) => {
+                println!("part_a: failed!");
+                println!("\texpected: {}", meta.answer_a);
+                println!("\treceived: {}", received);
+            }
+            None => {
+                println!("part_a: unsolved!");
+            }
+        }
+
+        match sample_b.part_b() {
+            Some(received) if received == meta.answer_b => {
+                let start = std::time::Instant::now();
+                let answer = real.part_b();
+                let duration = start.elapsed();
+                println!("part_b:\t{}", answer.unwrap());
+                println!(" ** in:\t{:?}", duration);
+            }
+            Some(received) => {
+                println!("part_b: failed!");
+                println!("\texpected: {}", meta.answer_b);
+                println!("\treceived: {}", received);
+            }
+            None => {
+                println!("part_b: unsolved!");
+            }
+        }
+    }
 }
 
 #[derive(Clone)]
