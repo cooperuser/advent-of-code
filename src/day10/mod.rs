1b6bb910ba482c41ae80ecc984b050391da0e284
Cooper b. Anderson <cooper@cooperuser.dev>
2024-12-09 23:36:56 -0600

    Day 10

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -1,6 +1,15 @@
+use std::collections::{HashSet, VecDeque};
+
+use crate::{
+    direction::DIRS,
+    vector::{Vector, VectorMap},
+};
+
 pub struct Day {
     #[allow(dead_code)]
     raw: Vec<String>,
+    map: VectorMap<i64>,
+    zeros: Vec<Vector>,
 }
 
 impl crate::solution::Solution<i64> for Day {
@@ -9,21 +18,88 @@ impl crate::solution::Solution<i64> for Day {
             input: include_str!("input.txt").to_string(),
             sample_a: include_str!("input_sample.txt").to_string(),
             sample_b: include_str!("input_sample.txt").to_string(),
-            answer_a: 0,
-            answer_b: 0,
+            answer_a: 36,
+            answer_b: 81,
         }
     }
 
     fn new(raw: Vec<String>) -> Self {
-        Self { raw: raw.clone() }
+        let size = Vector::new_usize(raw[0].len(), raw.len());
+        let mut map: VectorMap<i64> = VectorMap::new(size);
+        let mut zeros = Vec::new();
+        for (y, line) in raw.iter().enumerate() {
+            for (x, c) in line.chars().enumerate() {
+                let pos = Vector::new_usize(x, y);
+                map.insert(pos, c as i64 - '0' as i64);
+                if c == '0' {
+                    zeros.push(pos);
+                }
+            }
+        }
+
+        Self {
+            raw: raw.clone(),
+            map,
+            zeros,
+        }
     }
 
     fn part_a(&self) -> Option<i64> {
-        None
+        let mut trails = 0;
+        for &pos in &self.zeros {
+            let mut nines: HashSet<Vector> = HashSet::new();
+            let mut deque: VecDeque<Vector> = VecDeque::new();
+            deque.push_front(pos);
+
+            while let Some(pos) = deque.pop_back() {
+                let height = self.map.get(pos).unwrap();
+                if height == 9 {
+                    nines.insert(pos);
+                    continue;
+                }
+
+                for &dir in DIRS {
+                    let next_pos = pos + dir;
+                    match self.map.get(next_pos) {
+                        Some(next) if next == height + 1 => deque.push_front(next_pos),
+                        _ => {}
+                    }
+                }
+            }
+
+            trails += nines.len();
+        }
+
+        Some(trails as i64)
     }
 
     fn part_b(&self) -> Option<i64> {
-        None
+        let mut trails = 0;
+        for &pos in &self.zeros {
+            let mut nines = 0;
+            let mut deque: VecDeque<Vector> = VecDeque::new();
+            deque.push_front(pos);
+
+            while let Some(pos) = deque.pop_back() {
+                let height = self.map.get(pos).unwrap();
+                if height == 9 {
+                    nines += 1;
+                    continue;
+                }
+
+                for &dir in DIRS {
+                    let next_pos = pos + dir;
+                    match self.map.get(next_pos) {
+                        Some(next) if next == height + 1 => deque.push_front(next_pos),
+                        _ => {}
+                    }
+                }
+            }
+
+            trails += nines;
+        }
+
+        Some(trails as i64)
     }
 }
 
