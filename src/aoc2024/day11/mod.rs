ad02f2bdf616e8f36d08b86d6d1d73f51b307ccf
Cooper b. Anderson <cooper@cooperuser.dev>
2024-12-10 23:51:02 -0600

    Day 11

--- src/aoc2024/day11/mod.rs
+++ src/aoc2024/day11/mod.rs
@@ -1,6 +1,9 @@
+use std::collections::HashMap;
+
 pub struct Day {
     #[allow(dead_code)]
     raw: Vec<String>,
+    stones: HashMap<i64, i64>,
 }
 
 impl crate::solution::Solution<i64> for Day {
@@ -9,21 +12,57 @@ impl crate::solution::Solution<i64> for Day {
             input: include_str!("input.txt").to_string(),
             sample_a: include_str!("input_sample.txt").to_string(),
             sample_b: include_str!("input_sample.txt").to_string(),
-            answer_a: 0,
-            answer_b: 0,
+            answer_a: 55312,
+            answer_b: 65601038650482,
         }
     }
 
     fn new(raw: Vec<String>) -> Self {
-        Self { raw: raw.clone() }
+        let mut stones = HashMap::new();
+        for number in raw[0].split_whitespace() {
+            stones.entry(number.parse().unwrap()).or_insert(1);
+        }
+        Self {
+            raw: raw.clone(),
+            stones,
+        }
     }
 
     fn part_a(&self) -> Option<i64> {
-        None
+        let mut stones = self.stones.clone();
+        for _ in 0..25 {
+            stones = Self::blink(&stones);
+        }
+        Some(stones.values().sum())
     }
 
     fn part_b(&self) -> Option<i64> {
-        None
+        let mut stones = self.stones.clone();
+        for _ in 0..75 {
+            stones = Self::blink(&stones);
+        }
+        Some(stones.values().sum())
+    }
+}
+
+impl Day {
+    fn blink(stones: &HashMap<i64, i64>) -> HashMap<i64, i64> {
+        let mut after = HashMap::new();
+        for (&stone, &count) in stones.iter() {
+            if stone == 0 {
+                *after.entry(1).or_insert(0) += count;
+            } else if (stone.ilog10() + 1) % 2 == 0 {
+                let digits = stone.ilog10() + 1;
+                let power = 10i64.pow(digits / 2);
+                let right = stone % power;
+                let left = (stone - right) / power;
+                *after.entry(left).or_insert(0) += count;
+                *after.entry(right).or_insert(0) += count;
+            } else {
+                *after.entry(stone * 2024).or_insert(0) += count;
+            }
+        }
+        after
     }
 }
 
