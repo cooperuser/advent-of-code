66d686c212823a54e94579c53eaa9ed9bf3e290c
Cooper b. Anderson <cooper@cooperuser.dev>
2024-12-14 03:18:56 -0600

    2023 Day 23

---
+++ src/aoc2023/day23/mod.rs
@@ -0,0 +1,125 @@
+use std::collections::{HashSet, VecDeque};
+
+use crate::{
+    direction::{Direction, DIRS},
+    vector::{Vector, VectorMap, VectorSet},
+};
+
+pub struct Day {
+    #[allow(dead_code)]
+    raw: Vec<String>,
+    grid: VectorMap<Tile>,
+    start: Vector,
+    end: Vector,
+    size: Vector,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Tile {
+    Forest,
+    Path,
+    Slope(Direction),
+}
+
+impl crate::solution::Solution<i64> for Day {
+    fn meta() -> crate::solution::Meta<i64> {
+        crate::solution::Meta::<i64> {
+            input: include_str!("input.txt").to_string(),
+            sample_a: include_str!("input_sample.txt").to_string(),
+            sample_b: include_str!("input_sample.txt").to_string(),
+            answer_a: 94,
+            answer_b: 154,
+        }
+    }
+
+    fn new(raw: Vec<String>) -> Self {
+        let size = Vector::new_usize(raw[0].len(), raw.len());
+        let mut grid = VectorMap::new(size);
+        let mut start = None;
+        let mut end = None;
+        for (y, line) in raw.iter().enumerate() {
+            for (x, c) in line.chars().enumerate() {
+                let pos = Vector::new_usize(x, y);
+                if y == 0 && c == '.' {
+                    start = Some(pos);
+                } else if y as i64 == size.y - 1 && c == '.' {
+                    end = Some(pos);
+                }
+                grid.insert(pos, c.to_string().parse().unwrap());
+            }
+        }
+
+        Self {
+            raw: raw.clone(),
+            grid,
+            start: start.unwrap(),
+            end: end.unwrap(),
+            size,
+        }
+    }
+
+    fn part_a(&self) -> Option<i64> {
+        let mut deque: VecDeque<(i64, Vector, Direction, HashSet<Vector>)> =
+            VecDeque::from([(0, self.start, Direction::South, HashSet::new())]);
+        let mut seen: VectorMap<i64> = VectorMap::new(self.size);
+        let mut max = 0;
+        while let Some((distance, pos, last_dir, mut set)) = deque.pop_front() {
+            if !set.insert(pos) {
+                continue;
+            }
+            if let Some(last) = seen.get(pos) {
+                if last > distance {
+                    continue;
+                }
+            }
+            seen.insert(pos, distance);
+            if pos.y == self.size.y - 1 && distance > max {
+                max = distance;
+                continue;
+            }
+
+            if let Some(Tile::Slope(slope)) = self.grid.get(pos) {
+                deque.push_back((distance + 1, pos + slope, slope, set.clone()));
+                continue;
+            }
+            for dir in DIRS {
+                if dir.flip() == last_dir {
+                    continue;
+                }
+                let next = pos + dir;
+                match self.grid.get(pos) {
+                    Some(Tile::Path) => {
+                        deque.push_back((distance + 1, next, dir, set.clone()));
+                    }
+                    Some(Tile::Slope(slope)) if slope == dir => {
+                        deque.push_back((distance + 1, next, dir, set.clone()));
+                    }
+                    _ => {}
+                }
+            }
+        }
+        Some(max)
+    }
+
+    fn part_b(&self) -> Option<i64> {
+        None
+    }
+}
+
+impl std::str::FromStr for Tile {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "#" => Ok(Self::Forest),
+            "." => Ok(Self::Path),
+            "^" => Ok(Self::Slope(Direction::North)),
+            "v" => Ok(Self::Slope(Direction::South)),
+            ">" => Ok(Self::Slope(Direction::East)),
+            "<" => Ok(Self::Slope(Direction::West)),
+            _ => Err(()),
+        }
+    }
+}
+
+crate::solution::test_solution!();
--- src/aoc2023/mod.rs
+++ src/aoc2023/mod.rs
@@ -2,5 +2,6 @@ use crate::{solution::Run, Solution};
 
 mod day21;
 mod day22;
+mod day23;
 
-pub const DAYS: &[fn(bool) -> Run] = &[day21::Day::run, day22::Day::run];
+pub const DAYS: &[fn(bool) -> Run] = &[day21::Day::run, day22::Day::run, day23::Day::run];
