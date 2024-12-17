d50fefc9a19cf1d14a899953c1b43bdbb781a8f3
Cooper b. Anderson <cooper@cooperuser.dev>
2024-12-16 21:30:55 -0600

    Prepare for 2023 day 25

--- src/aoc2023/mod.rs
+++ src/aoc2023/mod.rs
@@ -4,10 +4,12 @@ mod day21;
 mod day22;
 mod day23;
 mod day24;
+mod day25;
 
 pub const DAYS: &[fn(bool) -> Run] = &[
     day21::Day::run,
     day22::Day::run,
     day23::Day::run,
     day24::Day::run,
+    day25::Day::run,
 ];
