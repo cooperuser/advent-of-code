5249120930d823f652576be31c98e28ff3472d4a
Cooper b. Anderson <cooper@cooperuser.dev>
2024-12-19 00:35:31 -0600

    Day 19 cleanup

--- src/aoc2024/day19/mod.rs
+++ src/aoc2024/day19/mod.rs
@@ -1,16 +1,13 @@
-use std::{
-    collections::{BinaryHeap, HashMap},
-    str::FromStr,
-};
+use std::collections::HashMap;
 
 pub struct Day {
     #[allow(dead_code)]
     raw: Vec<String>,
     towels: Vec<Vec<Color>>,
-    patterns: HashMap<Color, BinaryHeap<Pattern>>,
+    patterns: HashMap<Color, Vec<Pattern>>,
 }
 
-#[derive(Eq, PartialEq, Hash, Clone, Copy, Debug)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 enum Color {
     White,
     Blue,
@@ -20,12 +17,7 @@ enum Color {
 }
 
 #[derive(Clone, Eq, PartialEq)]
-struct Pattern {
-    pattern: Vec<Color>,
-}
-
-type Memo = HashMap<Vec<Color>, Option<Vec<Vec<Color>>>>;
-type Memo2 = HashMap<Vec<Color>, usize>;
+struct Pattern(Vec<Color>);
 
 impl crate::solution::Solution<i64, i64> for Day {
     fn meta() -> crate::solution::Meta<i64, i64> {
@@ -42,20 +34,21 @@ impl crate::solution::Solution<i64, i64> for Day {
         let (patterns, towels) = raw.split_once(|line| line.is_empty()).unwrap();
         let patterns: Vec<Pattern> = patterns[0]
             .split(", ")
-            .map(|p| p.chars().map(|c| c.to_string().parse().unwrap()).collect())
-            .map(|p| Pattern { pattern: p })
+            .map(|p| p.chars().map(|c| c.into()).collect())
+            .map(Pattern)
             .collect();
-        let mut sorted: HashMap<Color, BinaryHeap<Pattern>> = HashMap::new();
+        let mut sorted: HashMap<Color, Vec<Pattern>> = HashMap::new();
         for pattern in &patterns {
             sorted
-                .entry(pattern.pattern[0])
+                .entry(pattern.0[0])
                 .or_default()
                 .push(pattern.clone());
         }
         let towels = towels
             .iter()
-            .map(|t| t.chars().map(|c| c.to_string().parse().unwrap()).collect())
+            .map(|t| t.chars().map(|c| c.into()).collect())
             .collect();
+
         Self {
             raw: raw.clone(),
             towels,
@@ -65,9 +58,9 @@ impl crate::solution::Solution<i64, i64> for Day {
 
     fn part_a(&self) -> Option<i64> {
         let mut count = 0;
-        let mut memo: Memo = HashMap::new();
+        let mut memo = HashMap::new();
         for towel in &self.towels {
-            if self.is_towel_possible(towel, &mut memo).is_some() {
+            if self.is_towel_possible(towel, &mut memo) {
                 count += 1;
             }
         }
@@ -76,44 +69,43 @@ impl crate::solution::Solution<i64, i64> for Day {
 
     fn part_b(&self) -> Option<i64> {
         let mut count = 0;
-        let mut memo: Memo2 = HashMap::new();
+        let mut memo = HashMap::new();
         for towel in &self.towels {
-            count += self.get_towel_possibilities(towel, &mut memo);
+            count += self.get_possibilities(towel, &mut memo);
         }
         Some(count as i64)
     }
 }
 
 impl Day {
-    fn is_towel_possible(&self, towel: &[Color], memo: &mut Memo) -> Option<Vec<Vec<Color>>> {
-        if let Some(output) = memo.get(&towel.to_vec()) {
-            return output.clone();
+    fn is_towel_possible(&self, towel: &[Color], memo: &mut HashMap<Vec<Color>, bool>) -> bool {
+        if let Some(&output) = memo.get(&towel.to_vec()) {
+            return output;
         }
 
-        for Pattern { pattern: p } in self.patterns.get(&towel[0])? {
-            if p == towel {
-                return Some(vec![p.clone()]);
-            } else if p.len() > towel.len() {
+        let sorted = match self.patterns.get(&towel[0]) {
+            Some(s) => s,
+            None => return false,
+        };
+
+        for Pattern(p) in sorted {
+            if p.len() > towel.len() {
                 continue;
-            } else if *p == towel[0..p.len()] {
-                match self.is_towel_possible(&towel[p.len()..], memo) {
-                    Some(mut output) => {
-                        output.push(p.clone());
-                        memo.insert(towel.to_vec(), Some(output.clone()));
-                        return Some(output);
-                    }
-                    None => continue,
-                }
+            }
+            if p == towel
+                || *p == towel[0..p.len()] && self.is_towel_possible(&towel[p.len()..], memo)
+            {
+                return true;
             }
         }
 
-        memo.insert(towel.to_vec(), None);
-        None
+        memo.insert(towel.to_vec(), false);
+        false
     }
 
-    fn get_towel_possibilities(&self, towel: &[Color], memo: &mut Memo2) -> usize {
-        if let Some(output) = memo.get(&towel.to_vec()) {
-            return *output;
+    fn get_possibilities(&self, towel: &[Color], memo: &mut HashMap<Vec<Color>, usize>) -> usize {
+        if let Some(&output) = memo.get(&towel.to_vec()) {
+            return output;
         }
 
         let sorted = match self.patterns.get(&towel[0]) {
@@ -122,13 +114,13 @@ impl Day {
         };
 
         let mut possible = 0;
-        for Pattern { pattern: p } in sorted {
+        for Pattern(p) in sorted {
             if p == towel {
                 possible += 1;
             } else if p.len() > towel.len() {
                 continue;
             } else if *p == towel[0..p.len()] {
-                possible += self.get_towel_possibilities(&towel[p.len()..], memo)
+                possible += self.get_possibilities(&towel[p.len()..], memo)
             }
         }
 
@@ -137,24 +129,22 @@ impl Day {
     }
 }
 
-impl FromStr for Color {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.chars().next().unwrap() {
-            'w' => Ok(Self::White),
-            'u' => Ok(Self::Blue),
-            'b' => Ok(Self::Black),
-            'r' => Ok(Self::Red),
-            'g' => Ok(Self::Green),
-            _ => Err(()),
+impl From<char> for Color {
+    fn from(value: char) -> Self {
+        match value {
+            'w' => Self::White,
+            'u' => Self::Blue,
+            'b' => Self::Black,
+            'r' => Self::Red,
+            'g' => Self::Green,
+            _ => panic!("Unrecognized color {value}"),
         }
     }
 }
 
 impl Ord for Pattern {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.pattern.len().cmp(&other.pattern.len()).reverse()
+        self.0.len().cmp(&other.0.len()).reverse()
     }
 }
 
