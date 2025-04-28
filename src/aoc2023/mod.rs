dba12fa64ecccee00cfe5615233e22ece3bfff16
Cooper b. Anderson <cooper@cooperuser.dev>
2025-04-28 18:13:05 -0500

    Prepare to redo 2023 day 02

--- src/aoc2023/mod.rs
+++ src/aoc2023/mod.rs
@@ -1,6 +1,7 @@
 use crate::{solution::Run, Solution};
 
 mod day01;
+mod day02;
 mod day21;
 mod day22;
 mod day23;
@@ -9,6 +10,7 @@ mod day25;
 
 pub const DAYS: &[fn(bool) -> Run] = &[
     day01::Day::run,
+    day02::Day::run,
     day21::Day::run,
     day22::Day::run,
     day23::Day::run,
