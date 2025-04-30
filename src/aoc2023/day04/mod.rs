72b728e4f32e101546742cfed69b4b911a1b4742
Cooper b. Anderson <cooper@cooperuser.dev>
2025-04-30 14:36:36 -0500

    Redo 2023 day 04 part a

--- src/aoc2023/day04/mod.rs
+++ src/aoc2023/day04/mod.rs
@@ -1,6 +1,15 @@
+use std::collections::HashSet;
+
 pub struct Day {
     #[allow(dead_code)]
     raw: Vec<String>,
+    cards: Vec<Card>,
+}
+
+struct Card {
+    winning: HashSet<u8>,
+    numbers: HashSet<u8>,
+    points: usize,
 }
 
 impl crate::solution::Solution<i64, i64> for Day {
@@ -9,17 +18,45 @@ impl crate::solution::Solution<i64, i64> for Day {
             input: include_str!("input.txt").to_string(),
             sample_a: include_str!("input_sample.txt").to_string(),
             sample_b: include_str!("input_sample.txt").to_string(),
-            answer_a: 0,
+            answer_a: 13,
             answer_b: 0,
         }
     }
 
     fn new(raw: Vec<String>) -> Self {
-        Self { raw: raw.clone() }
+        let mut cards = Vec::new();
+        for line in raw.iter() {
+            let parts = line.split_once(": ").unwrap().1;
+            let (winning, numbers) = parts.split_once(" | ").unwrap();
+            let winning: HashSet<u8> = winning
+                .split_whitespace()
+                .map(|n| n.parse().unwrap())
+                .collect();
+            let numbers: HashSet<u8> = numbers
+                .split_whitespace()
+                .map(|n| n.parse().unwrap())
+                .collect();
+            let points = winning.intersection(&numbers).count();
+            cards.push(Card {
+                winning,
+                numbers,
+                points,
+            });
+        }
+
+        Self {
+            raw: raw.clone(),
+            cards,
+        }
     }
 
     fn part_a(&self) -> Option<i64> {
-        None
+        Some(
+            self.cards
+                .iter()
+                .map(|c| 2i64.pow(c.points as u32 - 1))
+                .sum(),
+        )
     }
 
     fn part_b(&self) -> Option<i64> {
