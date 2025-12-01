3d36f2c5501af04dce004c8e99cf58e130915cb7
Cooper b. Anderson <cooper@cooperuser.dev>
2025-11-30 23:22:04 -0600

    Day 01 part a

--- aoc2025/day01/src/lib.rs
+++ aoc2025/day01/src/lib.rs
@@ -3,6 +3,7 @@ use utils::prelude::*;
 pub struct Day {
     #[allow(dead_code)]
     raw: Vec<Rc<str>>,
+    rotations: Vec<i64>,
 }
 
 impl Solution<i64, i64> for Day {
@@ -11,17 +12,31 @@ impl Solution<i64, i64> for Day {
             input: include_str!("input.txt").to_string(),
             sample_a: include_str!("input_sample.txt").to_string(),
             sample_b: include_str!("input_sample.txt").to_string(),
-            answer_a: 0,
+            answer_a: 3,
             answer_b: 0,
         }
     }
 
     fn new(raw: Vec<Rc<str>>) -> Self {
-        Self { raw }
+        let mut rotations = Vec::new();
+        for line in &raw {
+            let (l, r) = line.split_at(1);
+            let value: i64 = r.parse().unwrap();
+            rotations.push(value * if l == "R" { 1 } else { -1 });
+        }
+        Self { raw, rotations }
     }
 
     fn part_a(&self) -> Option<i64> {
-        None
+        let mut dial = 50;
+        let mut count = 0;
+        for rot in &self.rotations {
+            dial += rot;
+            if dial % 100 == 0 {
+                count += 1;
+            }
+        }
+        Some(count)
     }
 
     fn part_b(&self) -> Option<i64> {
