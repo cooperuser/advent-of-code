b3d6906c3c0403420ebc85173b6a6cc1a10f2523
Cooper b. Anderson <cooper@cooperuser.dev>
2025-12-03 00:28:35 -0600

    Prepare for day 04

--- src/aoc2025.rs
+++ src/aoc2025.rs
@@ -4,7 +4,7 @@ pub const DAYS: &[fn(bool) -> Run] = &[
     aoc2025_day01::Day::run,
     aoc2025_day02::Day::run,
     aoc2025_day03::Day::run,
-    // aoc2025_day04::Day::run,
+    aoc2025_day04::Day::run,
     // aoc2025_day05::Day::run,
     // aoc2025_day06::Day::run,
     // aoc2025_day07::Day::run,
