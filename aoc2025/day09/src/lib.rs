23ffe76a11678102804b9b97732e0dbfb81c30b7
Cooper b. Anderson <cooper@cooperuser.dev>
2025-12-08 18:27:28 -0600

    Prepare for day 09

---
+++ aoc2025/day09/src/lib.rs
@@ -0,0 +1,32 @@
+use utils::prelude::*;
+
+pub struct Day {
+    #[allow(dead_code)]
+    raw: Vec<Rc<str>>,
+}
+
+impl Solution<i64, i64> for Day {
+    fn meta() -> Meta<i64, i64> {
+        Meta::<i64, i64> {
+            input: include_str!("input.txt").to_string(),
+            sample_a: include_str!("input_sample.txt").to_string(),
+            sample_b: include_str!("input_sample.txt").to_string(),
+            answer_a: 0,
+            answer_b: 0,
+        }
+    }
+
+    fn new(raw: Vec<Rc<str>>) -> Self {
+        Self { raw }
+    }
+
+    fn part_a(&self) -> Option<i64> {
+        None
+    }
+
+    fn part_b(&self) -> Option<i64> {
+        None
+    }
+}
+
+utils::solution::test_solution!(aoc2025, day09);
